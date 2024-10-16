ab0b42eeb230a950aee3c0e53efd72c36cb79a27
Akala Moses <akoredeolamilekan97@gmail.com>
2024-10-16 12:24:17 +0000

    minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,19 +4,19 @@ A method that calculates the fewest number of operations needed to result
 """
 
 
-def minOperations(n):
-    if n <= 1:
-        return 0
-    
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    clipboard = 'H'
+    current_text = 'H'
     operations = 0
-    divisor = 2
-    
-    # Factorize n by dividing it by the smallest divisor until we reach 1
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-    
+    while len(current_text) < n:
+        if n % len(current_text) == 0:
+            operations += 2
+            clipboard = current_text
+            current_text += current_text
+        else:
+            operations += 1
+            current_text += clipboard
+    if len(current_text) != n:
+        return 0
     return operations
-
